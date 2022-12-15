18adad25f8aea86c8c99909899ad0b61c9ed2637
wyzlhf <haifeng0130@126.com>
2022-12-15 10:28:44 +0800

    添加了前端交互

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -195,8 +195,32 @@ app.get('/consensus',function (req,res) {
         }
     })
 })
+app.get('/block/:blockHash',function (req, res) {
+    const blockHash=req.params.blockHash
+    const correctBlock=bitcoin.getBlock(blockHash)
+    res.json({
+        block:correctBlock
+    })
+})
+app.get('/block/:transactionId',function (req, res) {
+    const transactionId=req.params.transactionId
+    const transactionData=bitcoin.getTransaction(transactionId)
+    res.json({
+        transaction:transactionData.transaction,
+        block:transactionData.block
+    })
+})
+app.get('/address/:address',function (req, res) {
+    const address=req.params.address
+    const addressData=bitcoin.getAddressData(address)
+    res.json({
+        addressData:addressData
+    })
+})
 
-
+app.get('/block-explorer',function (req, res) {
+    res.sendFile('./block-explorer/index.html',{root:__dirname})
+})
 
 app.listen(port, function () {
     console.log(`Listening on port ${port}...`)
